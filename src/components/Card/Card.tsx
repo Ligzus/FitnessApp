60e2b26c6a24863d50b71abd5bae3ca006e86c21
Invior <twhlost@gmail.com>
2024-09-01 00:10:13 +0400

    Поправил расположение картинок в проекте

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,10 +2,10 @@ function Card() {
 	return (
 		<div className="card w-[360px] bg-white rounded-[30px] flex flex-col gap-6 shadow-[0_4px_67px_-12px_rgba(0,0,0,0.13)]">
 			<div className="cardImage relative">
-				<img className="" src="src/assets/yoga-card.png" alt="yoga" />
+				<img className="" src="./yoga-card.png" alt="yoga" />
 				<button className="addCourse w-[32px] h-[32px] absolute top-5 right-5" title="Добавить курс">
 					<svg className="w-[32px] h-[32px]">
-						<use xlinkHref="src/assets/icon/sprite.svg#icon-plus" />
+						<use xlinkHref="./icon/sprite.svg#icon-plus" />
 					</svg>
 				</button>
 			</div>
@@ -16,19 +16,19 @@ function Card() {
 				<div className="courseParams flex flex-row flex-wrap gap-1.5 mb-[15px]">
 					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
 						<svg className="w-[15px] h-[15px]">
-							<use xlinkHref="src/assets/icon/sprite.svg#icon-calendar" />
+							<use xlinkHref="./icon/sprite.svg#icon-calendar" />
 						</svg>
 						25 дней
 					</p>
 					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
 						<svg className="w-[15px] h-[15px]">
-							<use xlinkHref="src/assets/icon/sprite.svg#icon-time" />
+							<use xlinkHref="./icon/sprite.svg#icon-time" />
 						</svg>
 						20-50 мин/день
 					</p>
 					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
 						<svg className="w-[18px] h-[18px]">
-							<use xlinkHref="src/assets/icon/sprite.svg#icon-complexity" />
+							<use xlinkHref="./icon/sprite.svg#icon-complexity" />
 						</svg>
 						Сложность
 					</p>
