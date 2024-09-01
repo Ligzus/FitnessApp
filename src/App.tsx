0160e7b221402a190cdbd8b3518a4ab702706ad0
Ligzus2 <infopop@mail.ru>
2024-09-01 19:41:21 +0300

    http://localhost:5173/profile - профиль пользователя. В Header добавил состояние, его доработаем. Добавил роутер.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import "./App.css";
-import Main from "./app/Main/Main.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./app/Main/Main";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  return <Main />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,59 @@
+import UserCards from "../Card/UserCards/UserCards";
+import Header from "../Header/Header";
+
+function Profile() {
+    return (
+        <div>
+            <Header />
+            <div className="mt-[60px]">
+                <h2 className="text-[40px] mb-[40px] font-medium text-left leading-none">
+                    Профиль
+                </h2>
+            
+                <div className="flex bg-[#FFFFFF] rounded-[28px]">
+                <div className="px-[30px] py-[30px]">
+                    <img src="./profile-photo.svg" alt="profile-photo" />
+                </div>
+            
+                <div className="px-[33px] py-[30px]">
+                    <p className="text-[32px] font-medium text-start mb-[30px]">
+                    Сергей
+                    </p>
+
+                    <div className="flex flex-col items-start mb-[30px]">
+                        <p>
+                            Логин: sergey.petrov96
+                        </p>
+                        <p>
+                            Пароль: 4fkhdj880d
+                        </p>
+                    </div>
+
+                    <div className="flex gap-[10px]">
+                    <button className="w-[192px] h-[52px] bg-[#BCEC30] rounded-[46px] hover:bg-[#C6FF00] active:bg-[#000000] active:text-[#FFFFFF] text-lg">
+                            Изменить пароль
+                        </button>
+                        <button className="w-[192px] h-[52px] border border-black bg-[#ffffff] rounded-[46px] hover:bg-[#E9ECED] active:bg-[#000000] active:text-[#FFFFFF] text-lg">
+                            Выйти
+                        </button>
+                    </div>
+                
+                </div>
+            </div>   
+        </div>
+        
+        <div className="mt-[60px]">
+            <h2 className="text-[40px] mb-[40px] font-medium text-left leading-none">
+                Мои курсы
+            </h2>
+            <div className="coursCards flex flex-row flex-wrap gap-10">
+                <UserCards />
+                <UserCards />
+                <UserCards />
+            </div>
+        </div>
+
+    </div>); 
+}
+
+export default Profile;
