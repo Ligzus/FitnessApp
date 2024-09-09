af699e5e17988555299fde46dcf89019ed6f5668
Ligzus2 <infopop@mail.ru>
2024-09-10 00:10:48 +0300

    поправил путь к лого в модулях

--- src/components/Modal/AuthModal/Register/Register.tsx
+++ src/components/Modal/AuthModal/Register/Register.tsx
@@ -41,7 +41,7 @@ const Register: React.FC<ModalProps> = ({ closeModal, toggleModal }) => {
 				className="relative flex flex-col items-center p-10 gap-12 w-[360px] h-[auto] bg-white shadow-[0_4px_67px_-12px_rgba(0,0,0,0.13)] rounded-[30px]"
 				onClick={(e) => e.stopPropagation()}
 			>
-				<img src="./logo.svg" alt="logo" />
+				<img src="/logo.svg" alt="logo" />
 
 				<div className="flex flex-col items-center gap-8 w-[280px] h-[auto]">
 					<div className="flex flex-col items-start gap-[10px] w-[280px]">
