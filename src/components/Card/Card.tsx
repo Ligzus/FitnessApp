6dae53126b57f722b4f463d13cdb65c8754754ed
Invior <twhlost@gmail.com>
2024-09-10 01:09:53 +0400

    Перенесена логика отображения карточек курсов в Main

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,82 +1,46 @@
 import { Link } from "react-router-dom";
-import { getCourse } from "../../utils/api";
-import { useEffect, useState } from "react";
+import { CardType } from "../../types/cards";
 
-function Card() {
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [courses, setCourses] = useState<any[]>([]);
-
-	useEffect(() => {
-		getCourse()
-			.then((data: any) => {
-				let coursesData: any[] = [];
-				for (let i = 0; i < Object.keys(data).length; i++) {
-					coursesData.push(data[Object.keys(data)[i]]);
-				}
-				setCourses(coursesData);
-			})
-			.catch((error: any) => {
-				console.warn(error);
-			})
-			.finally(() => {
-				setIsLoaded(true);
-			});
-	}, []);
-
-	console.log(courses);
-
+function Card({ courseId, image, nameRu }: CardType) {
 	return (
-		<div className="coursCards">
-			{isLoaded ? (
-				<div className="flex justify-center xl:justify-start flex-wrap gap-6 sm:gap-10">
-					{courses.map((course: any) => (
-						<Link
-							to={`/course/${course._id}`} // Передаем ID курса в URL
-							className="card w-[343px] lg:w-[360px] bg-white rounded-[30px] flex flex-col gap-6 shadow-[0_4px_67px_-12px_rgba(0,0,0,0.13)]"
-							key={course._id}
-						>
-							<div className="cardImage relative">
-								<img className="" src={course.images.cardImage} alt={course.nameRU} />
-								<button className="addCourse w-[32px] h-[32px] absolute top-5 right-5" title="Добавить курс">
-									<svg className="w-[32px] h-[32px]">
-										<use xlinkHref="./icon/sprite.svg#icon-plus" />
-									</svg>
-								</button>
-							</div>
-							<div className="parameters flex flex-col gap-5 mx-[30px]">
-								<div className="courseTitle flex justify-start">
-									<h3 className="text-[24px] sm:text-[32px] font-medium text-left">{course.nameRU}</h3>
-								</div>
-								<div className="courseParams flex flex-row flex-wrap gap-1.5 mb-[15px]">
-									<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
-										<svg className="w-[15px] h-[15px]">
-											<use xlinkHref="./icon/sprite.svg#icon-calendar" />
-										</svg>
-										25 дней
-									</p>
-									<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
-										<svg className="w-[15px] h-[15px]">
-											<use xlinkHref="./icon/sprite.svg#icon-time" />
-										</svg>
-										20-50 мин/день
-									</p>
-									<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
-										<svg className="w-[18px] h-[18px]">
-											<use xlinkHref="./icon/sprite.svg#icon-complexity" />
-										</svg>
-										Сложность
-									</p>
-								</div>
-							</div>
-						</Link>
-					))}
+		<Link
+			to={`/course/${courseId}`} // Передаем ID курса в URL
+			className="card w-[343px] lg:w-[360px] bg-white rounded-[30px] flex flex-col gap-6 shadow-[0_4px_67px_-12px_rgba(0,0,0,0.13)]" key={courseId}
+		>
+			<div className="cardImage relative">
+				<img className="" src={image} alt={nameRu} />
+				<button className="addCourse w-[32px] h-[32px] absolute top-5 right-5" title="Добавить курс">
+					<svg className="w-[32px] h-[32px]">
+						<use xlinkHref="./icon/sprite.svg#icon-plus" />
+					</svg>
+				</button>
+			</div>
+			<div className="parameters flex flex-col gap-5 mx-[30px]">
+				<div className="courseTitle flex justify-start">
+					<h3 className="text-[24px] sm:text-[32px] font-medium text-left">{nameRu}</h3>
 				</div>
-			) : (
-				<div>
-					<p>Страница загружается</p>
+				<div className="courseParams flex flex-row flex-wrap gap-1.5 mb-[15px]">
+					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
+						<svg className="w-[15px] h-[15px]">
+							<use xlinkHref="./icon/sprite.svg#icon-calendar" />
+						</svg>
+						25 дней
+					</p>
+					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
+						<svg className="w-[15px] h-[15px]">
+							<use xlinkHref="./icon/sprite.svg#icon-time" />
+						</svg>
+						20-50 мин/день
+					</p>
+					<p className="parameter bg-[#F7F7F7] p-2.5 rounded-full flex flex-row gap-1.5 items-center">
+						<svg className="w-[18px] h-[18px]">
+							<use xlinkHref="./icon/sprite.svg#icon-complexity" />
+						</svg>
+						Сложность
+					</p>
 				</div>
-			)}
-		</div>
+			</div>
+		</Link>
 	);
 }
 
