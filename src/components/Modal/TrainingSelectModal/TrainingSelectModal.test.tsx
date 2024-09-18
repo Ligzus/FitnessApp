c34558c8e402a9617b4b393bcce9e19c7f052f6a
Ligzus2 <infopop@mail.ru>
2024-09-18 10:45:35 +0300

    + обновленный тест TrainingSelectModal

--- src/components/Modal/TrainingSelectModal/TrainingSelectModal.test.tsx
+++ src/components/Modal/TrainingSelectModal/TrainingSelectModal.test.tsx
@@ -1,53 +1,19 @@
-import { render, screen, act } from "@testing-library/react";
-import TrainingSelectModal from "./TrainingSelectModal";
-import { MemoryRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
+import { render } from '@testing-library/react';
+import TrainingSelectModal from './TrainingSelectModal';
+import '@testing-library/jest-dom/';
 
 // Мокируем функции для API
-jest.mock("../../../utils/api", () => ({
-	getCourseById: jest.fn().mockResolvedValue({
-		workouts: ["workout1", "workout2"],
-	}),
-	getWorkoutsById: jest
-		.fn()
-		.mockResolvedValue({
-			_id: "workout1",
-			name: "Workout 1",
-		})
-		.mockResolvedValueOnce({
-			_id: "workout2",
-			name: "Workout 2",
-		}),
+jest.mock('../../../utils/api', () => ({
+    getCourseById: jest.fn().mockResolvedValue({ workouts: ['workout1', 'workout2'] }),
+    getWorkoutsById: jest.fn().mockResolvedValue({ _id: 'workout1', name: 'Workout 1' }),
 }));
 
-describe("TrainingSelectModal", () => {
-	it("должен правильно рассчитывать visitedRatio и передавать его в onVisitedRatioChange", async () => {
-		const mockCloseModal = jest.fn();
-		const mockOnVisitedRatioChange = jest.fn();
+describe('TrainingSelectModal', () => {
+    it('matches the snapshot', () => {
+        const { asFragment } = render(
+            <TrainingSelectModal closeModal={() => {}} courseId="course1" />
+        );
 
-		await act(async () => {
-			render(
-				<MemoryRouter>
-					<TrainingSelectModal
-						closeModal={mockCloseModal}
-						courseId="course1"
-						onVisitedRatioChange={mockOnVisitedRatioChange}
-					/>
-				</MemoryRouter>,
-			);
-		});
-
-		// Проверка наличия текста
-		expect(screen.getByText("Выберите тренировку")).toBeInTheDocument();
-
-		// Имитация посещения тренировки
-		const links = screen.getAllByText(/Workout/i);
-		act(() => {
-			// Эмулируем клик по первой тренировке
-			links[0].click();
-		});
-
-		// Проверка вызова mockOnVisitedRatioChange
-		expect(mockOnVisitedRatioChange).toHaveBeenCalledWith("50"); // В зависимости от того, сколько тренировок было посещено
-	});
+        expect(asFragment()).toMatchSnapshot();
+    });
 });
